252a2a29d64396fcc7c52331e59fa2c3c552e0bf
TotalCAE <rod@totalcae.com>
2016-10-24 13:37:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ localip=`hostname -i | cut --delimiter='.' -f -3`
 
 mkdir -p /srv/ 
 cd /srv
-/bin/wget https://totalcaeapplications.blob.core.windows.net/applications/chef-repo.gpg
+/bin/wget https://totalcaeapplications.blob.core.windows.net/applications/chef-repo.tar.gpg
 echo ${KEY} |  gpg --batch --output chef.tar --passphrase-fd O  --decrypt chef-repo.tar.gpg
 tar xfv chef.tar
 cd chef-repo
