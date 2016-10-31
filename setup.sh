a00ca535c2114f1cc137a7f24cd735d68f596f06
TotalCAE <rod@totalcae.com>
2016-10-30 23:30:41 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,23 @@
+
+
+# TotalCAE CONFIDENTIAL
+#
+#
+# 2016 TotalCAE
+# All Rights Reserved.
+#
+# NOTICE: All information contained herein is, and remains
+# the property of TotalCAE
+#
+# The intellectual and technical concepts contained
+# herein are proprietary to TotalCAE
+# and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from TotalCAE
+#
+
 export USER=$1
 export PASS=$2
 export KEY=$3
